8d7cef5113b33eb9fa4b6ac33d0c972bf324d538
HanLinAung88 <hanlaung@stanford.edu>
2020-03-07 22:57:38 -0800

    Added and fixed dataloader for coral

--- dataloaders/coral.py
+++ dataloaders/coral.py
@@ -15,32 +15,37 @@ class CoralDataset(BaseDataSet):
 	Coral dataset
     """
     def __init__(self, **kwargs):
-        self.num_classes = 3
+        self.num_classes = 2
         super(CoralDataset, self).__init__(**kwargs)
 
     def _set_files(self):
-        if self.split in  ["training", "validation"]:
-            self.image_dir = os.path.join(self.root, 'images', self.split)
-            self.label_dir = os.path.join(self.root, 'annotations', self.split)
-            self.files = [os.path.basename(path).split('.')[0] for path in glob(self.image_dir + '/*.jpg')]
+        if self.split in  ["train", "test"]:
+            self.image_dir = os.path.join(self.root, self.split, 'images')
+            self.label_dir = os.path.join(self.root, self.split, 'masks_ch1')
+            self.files = [os.path.basename(path).split('.')[0] for path in glob(self.image_dir + '/*.png')]
         else: raise ValueError(f"Invalid split name {self.split}")
     
     def _load_data(self, index):
         image_id = self.files[index]
-        image_path = os.path.join(self.image_dir, image_id + '.jpg')
-        label_path = os.path.join(self.label_dir, image_id + '.png')
+        image_path = os.path.join(self.image_dir, image_id + '.png')
+        label_path = os.path.join(self.label_dir, image_id + '_mask.png')
         image = np.asarray(Image.open(image_path).convert('RGB'), dtype=np.float32)
         label = np.asarray(Image.open(label_path), dtype=np.int32) - 1 # from -1 to 149
         return image, label, image_id
 
-class CoralDataLoader(BaseDataLoader):
+class Coral(BaseDataLoader):
     def __init__(self, data_dir, batch_size, split, crop_size=None, base_size=None, scale=True, num_workers=1, val=False,
                     shuffle=False, flip=False, rotate=False, blur= False, augment=False, val_split= None, return_id=False):
 
+        #TODO: Initialize to our dataset
+        self.MEAN = [0.43931922, 0.41310471, 0.37480941]
+        self.STD = [0.24272706, 0.23649098, 0.23429529]
 
         kwargs = {
             'root': data_dir,
             'split': split,
+            'mean': self.MEAN,
+            'std': self.STD,
             'augment': augment,
             'crop_size': crop_size,
             'base_size': base_size,
@@ -53,4 +58,4 @@ class CoralDataLoader(BaseDataLoader):
         }
 
         self.dataset = CoralDataset(**kwargs)
-        super(CoralDataLoader, self).__init__(self.dataset, batch_size, shuffle, num_workers, val_split)
+        super(Coral, self).__init__(self.dataset, batch_size, shuffle, num_workers, val_split)
