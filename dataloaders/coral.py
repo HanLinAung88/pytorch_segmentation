19b944a34e9acaf0574677b82ea8c7276ea26d11
HanLinAung88 <hanlaung@stanford.edu>
2020-03-18 17:07:40 -0700

    Fixed coral data loader

--- dataloaders/coral.py
+++ dataloaders/coral.py
@@ -22,16 +22,25 @@ class CoralDataset(BaseDataSet):
         if self.split in  ["train", "test"]:
             self.image_dir = os.path.join(self.root, self.split, 'images')
             self.label_dir = os.path.join(self.root, self.split, 'masks_ch1')
+            self.label_dir_2 = os.path.join(self.root, self.split, 'masks_ch4')
             self.files = [os.path.basename(path).split('.')[0] for path in glob(self.image_dir + '/*.png')]
         else: raise ValueError(f"Invalid split name {self.split}")
     
     def _load_data(self, index):
         image_id = self.files[index]
         image_path = os.path.join(self.image_dir, image_id + '.png')
+		# There are two corals we are detecting
         label_path = os.path.join(self.label_dir, image_id + '_mask.png')
+        label_path_2 = os.path.join(self.label_dir_2, image_id + '_mask.png')
+
         image = np.asarray(Image.open(image_path).convert('RGB'), dtype=np.float32)
-        label = np.asarray(Image.open(label_path), dtype=np.int32) - 1 # from -1 to 149
-        return image, label, image_id
+        label = np.asarray(Image.open(label_path), dtype=np.int32)  
+        label_2 = np.asarray(Image.open(label_path_2), dtype=np.int32)  
+
+        final_label = np.where(label != 0, label, label_2)
+
+        final_label[final_label == 255] = 1
+        return image, final_label, image_id
 
 class Coral(BaseDataLoader):
     def __init__(self, data_dir, batch_size, split, crop_size=None, base_size=None, scale=True, num_workers=1, val=False,
